31ab435b43a0754c79265b6f9cbe991e4bfc3207
yaelmosh <yaelmo30@gmail.com>
2026-09-11 12:50:11 +0300

    Basic Pacman movement

--- Assets/Scripts/Pacman.cs
+++ Assets/Scripts/Pacman.cs
@@ -1,16 +1,75 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
+[RequireComponent(typeof(Movement))]
 public class Pacman : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private Movement movement;
+    private LayerMask obstacleLayer;
+
+    void Awake()
     {
-        
+        movement = GetComponent<Movement>();
+        obstacleLayer = LayerMask.GetMask("Obstacle");
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        Keyboard keyboard = Keyboard.current;
+        if (keyboard == null)
+        {
+            return;
+        }
+
+        // For changing current movement direction
+        if (keyboard.wKey.isPressed || keyboard.upArrowKey.isPressed)
+        {
+            changeMovementDirection(Vector2.up);
+        }
+
+        else if (keyboard.sKey.isPressed || keyboard.downArrowKey.isPressed)
+        {
+            changeMovementDirection(Vector2.down);
+        }
+
+        else if (keyboard.aKey.isPressed || keyboard.leftArrowKey.isPressed)
+        {
+            changeMovementDirection(Vector2.left);
+        }
+
+        else if (keyboard.dKey.isPressed || keyboard.rightArrowKey.isPressed)
+        {
+            changeMovementDirection(Vector2.right);
+        }
+
+        // For stopping when hitting walls
+        if (movement.direction != Vector2.zero && isWallCollision(movement.direction))
+        {
+            movement.direction = Vector2.zero;
+        }
+    }
+
+    private void changeMovementDirection(Vector2 direction)
+    {
+        if (direction == Vector2.zero || direction == movement.direction)
+        {
+            return;
+        }
+
+        if (isWallCollision(direction))
+        {
+            return;
+        }
+
+        movement.direction = direction;
+
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0, 0, angle); // So the sprite gets rotated
+    }
+
+    private bool isWallCollision(Vector2 direction)
+    {
+        RaycastHit2D hit = Physics2D.BoxCast(transform.position, Vector2.one * 0.75f, 0, direction, 0.25f, obstacleLayer);
+        return hit.collider != null;
     }
 }
